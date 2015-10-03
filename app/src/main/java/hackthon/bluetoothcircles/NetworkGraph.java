ea8a49f844593c1a0dbd73ad1807ef3cd04e41e0
Sujay Kathrotia <sujaykathrotia@gmail.com>
2015-10-03 19:44:54 -0400

    tree code files added

---
+++ app/src/main/java/hackthon/bluetoothcircles/NetworkGraph.java
@@ -0,0 +1,13 @@
+package hackthon.bluetoothcircles;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Sujay on 10/3/2015.
+ */
+public class NetworkGraph {
+
+    public HashMap<String, List<String>> recievedMap;
+
+}
